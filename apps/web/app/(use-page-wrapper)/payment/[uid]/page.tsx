fad03b84d87ecbedc10f42694602f13062206568
agostino ruscio <36762365+XxGragUxx@users.noreply.github.com>
2026-06-12 19:46:40 +0200

    Refactor server-side data fetching in payment page

--- apps/web/app/(use-page-wrapper)/payment/[uid]/page.tsx
+++ apps/web/app/(use-page-wrapper)/payment/[uid]/page.tsx
@@ -1,25 +1,14 @@
-import { withAppDirSsr } from "@calcom/app-store/_utils/withAppDirSsr";
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import PaymentPage from "./PaymentPage";
 import { getServerSideProps } from "./_getServerSideProps";
 
-const getData = withAppDirSsr(getServerSideProps);
-
 export default async function Page({ params }: { params: { uid: string } }) {
-  const result = await getData({ params });
+  const result = await getServerSideProps({ params });
 
   if ("notFound" in result && result.notFound) {
     return notFound();
   }
 
-  if ("redirect" in result && result.redirect) {
-    return redirect(result.redirect.destination);
-  }
-
-  const { props } = result as Awaited<ReturnType<typeof getServerSideProps>> & {
-    props: NonNullable<unknown>;
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return <PaymentPage {...(props as any)} />;
+  const { props } = result as { props: Parameters<typeof PaymentPage>[0] };
+  return <PaymentPage {...props} />;
 }
