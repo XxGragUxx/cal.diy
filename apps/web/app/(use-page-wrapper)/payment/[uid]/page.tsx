9518405fb5663ad9638898ddb51ba50abffdd1e1
agostino ruscio <36762365+XxGragUxx@users.noreply.github.com>
2026-06-12 22:03:27 +0200

    Change Page function parameter to Promise type

--- apps/web/app/(use-page-wrapper)/payment/[uid]/page.tsx
+++ apps/web/app/(use-page-wrapper)/payment/[uid]/page.tsx
@@ -2,7 +2,8 @@ import { notFound } from "next/navigation";
 import PaymentPage from "./PaymentPage";
 import { getServerSideProps } from "./_getServerSideProps";
 
-export default async function Page({ params }: { params: { uid: string } }) {
+export default async function Page(componentProps: { params: Promise<{ uid: string }> }) {
+  const params = await componentProps.params;
   const result = await getServerSideProps({ params });
 
   if ("notFound" in result && result.notFound) {
